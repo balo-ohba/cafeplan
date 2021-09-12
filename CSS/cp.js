641edf5317ddfaf8471e235471763a7797e68a2d
balo-ohba <balo.trom817@gmail.com>
2021-09-13 04:44:52 +0900

    fix

--- CSS/cp.js
+++ CSS/cp.js
@@ -11,12 +11,10 @@ function FixedAnime() {
 			$('#header').removeClass('dnone');//dnoneというクラス名を除く
 		}
 }
-
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
 	FixedAnime();//スクロールをするとハンバーガーメニューに変化するための関数を呼ぶ
 });
-
 //ボタンをクリックした際のアニメーションの設定
 $(".openbtn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
@@ -27,15 +25,24 @@ $("#g-navi li a").click(function () {//ナビゲーションのリンクがク�
     $("#header").removeClass('panelactive');//ヘッダーのpanelactiveクラスも除去
 });
 
-//お悩みホバー
-$(function() {$('.lesson-hover').hover(
-    function() {
-      // 子要素の「.text-contents」の要素を取得し、text-activeクラスをつけてください
-      $(this).find('.text-contents').addClass('text-active');
-    },
-    function() {
-      // 子要素の「.text-contents」の要素を取得し、text-activeクラスを外してください
-      $(this).find('.text-contents').removeClass('text-active');
-    }
-  );
+//アプリ画面スライド
+$('.slider').slick({
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+	speed:1000,//スライドの動きのスピード。初期値は300。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 1,//スライドを画面に3枚見せる
+	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+	pauseOnFocus: false,//フォーカスで一時停止を無効
+	pauseOnHover: false,//マウスホバーで一時停止を無効
+	pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+$('.slider').slick('slickPlay');
 });
